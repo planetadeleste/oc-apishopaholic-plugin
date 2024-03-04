67b3eb9697bc05e03bf626ec15636f429b46ddd4
Alvaro Canepa <bfpdevel@gmail.com>
2024-03-04 09:39:34 -0300

    - Update lang api to newest RainLab.Translate plugin

--- routes/lang.php
+++ routes/lang.php
@@ -1,4 +1,5 @@
 <?php
+
 Route::prefix('lang')
     ->name('lang.')
     ->group(
@@ -8,14 +9,17 @@ Route::prefix('lang')
             Route::get('{lang?}', 'Langs@lang');
             Route::post('tr', 'Langs@missing');
 
-            Route::get('yaml', function () {
+            Route::get('yaml/{locale?}/{dump?}', function (string $locale = 'es', bool $dump = false) {
                 $arLangs = [];
-                \RainLab\Translate\Models\Message::query()
-                    ->orderBy('code')
-                    ->each(function (\RainLab\Translate\Models\Message $obModel) use (&$arLangs) {
-                        $sMessage = array_get($obModel->message_data, 'es');
-                        $arLangs[$obModel->code] = $sMessage;
-                    });
+                $obMessage =  \RainLab\Translate\Models\Message::where('locale', $locale)->first();
+                if ($obMessage) {
+                    $arLangs = $obMessage->data;
+                    ksort($arLangs);
+                }
+
+                if ($dump) {
+                    dd(json_encode($arLangs, JSON_PRETTY_PRINT));
+                }
 
                 return response()->json($arLangs, 200, [], JSON_PRETTY_PRINT);
             });
