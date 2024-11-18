437ac5d0c8c2a957d980bff1fe25b8f9d16cb74a
Alvaro Canepa <bfpdevel@gmail.com>
2024-11-18 09:50:14 -0300

    - Use function on resource data

--- classes/resource/offer/ItemResource.php
+++ classes/resource/offer/ItemResource.php
@@ -1,6 +1,9 @@
-<?php namespace PlanetaDelEste\ApiShopaholic\Classes\Resource\Offer;
+<?php
+
+namespace PlanetaDelEste\ApiShopaholic\Classes\Resource\Offer;
 
 use Lovata\Shopaholic\Classes\Helper\CurrencyHelper;
+use Lovata\Shopaholic\Classes\Item\OfferItem;
 use PlanetaDelEste\ApiShopaholic\Classes\Resource\File\IndexCollection as IndexCollectionImages;
 use PlanetaDelEste\ApiShopaholic\Plugin;
 use PlanetaDelEste\ApiToolbox\Classes\Resource\Base;
@@ -8,7 +11,8 @@ use PlanetaDelEste\ApiToolbox\Classes\Resource\Base;
 /**
  * Class ItemResource
  *
- * @mixin \Lovata\Shopaholic\Classes\Item\OfferItem
+ * @mixin   OfferItem
+ *
  * @package PlanetaDelEste\ApiShopaholic\Classes\Resource\Offer
  */
 class ItemResource extends Base
@@ -19,39 +23,39 @@ class ItemResource extends Base
     public function getData(): array
     {
         return [
-            'active'          => (bool)$this->active,
-            'preview_image'   => $this->preview_image ? $this->preview_image->getPath() : null,
-            'images'          => IndexCollectionImages::make(collect($this->images)),
-            'price_value'     => (float)$this->price_value,
-            'old_price_value' => (float)$this->old_price_value,
-            'currency'        => CurrencyHelper::instance()->getDefault()->symbol,
-            'thumbnail'       => $this->preview_image ? $this->preview_image->getThumb(
+            'active'          => fn() => (bool) $this->active,
+            'preview_image'   => fn() => $this->preview_image ? $this->preview_image->getPath() : null,
+            'images'          => fn() => IndexCollectionImages::make(collect($this->images)),
+            'price_value'     => fn() => (float) $this->price_value,
+            'old_price_value' => fn() => (float) $this->old_price_value,
+            'currency'        => static fn() => CurrencyHelper::instance()->getDefault()->symbol,
+            'thumbnail'       => fn() => $this->preview_image ? $this->preview_image->getThumb(
                 300,
                 300,
                 ['mode' => 'crop']
             ) : null,
-            'property'        => $this->formatProperty()
+            'property'        => fn() => $this->formatProperty()
         ];
     }
 
     public function getDataKeys(): array
     {
         return [
+            'active',
             'code',
             'currency',
             'currency_code',
             'id',
             'name',
-            'active',
+            'old_price',
+            'old_price_value',
             'preview_image',
             'preview_text',
             'price',
             'price_value',
-            'old_price',
-            'old_price_value',
             'product_id',
             'quantity',
-            'thumbnail'
+            'thumbnail',
         ];
     }
 
--- classes/resource/product/ItemResource.php
+++ classes/resource/product/ItemResource.php
@@ -1,17 +1,21 @@
-<?php namespace PlanetaDelEste\ApiShopaholic\Classes\Resource\Product;
+<?php
 
+namespace PlanetaDelEste\ApiShopaholic\Classes\Resource\Product;
+
+use Lovata\Shopaholic\Classes\Item\ProductItem;
 use PlanetaDelEste\ApiShopaholic\Classes\Resource\Brand\ItemResource as ItemResourceBrand;
-use PlanetaDelEste\ApiToolbox\Classes\Resource\Base as BaseResource;
 use PlanetaDelEste\ApiShopaholic\Classes\Resource\Category\ItemResource as ItemResourceCategory;
 use PlanetaDelEste\ApiShopaholic\Classes\Resource\File\IndexCollection as IndexCollectionImages;
 use PlanetaDelEste\ApiShopaholic\Classes\Resource\Offer\IndexCollection as IndexCollectionOffer;
 use PlanetaDelEste\ApiShopaholic\Plugin;
+use PlanetaDelEste\ApiToolbox\Classes\Resource\Base as BaseResource;
 use System\Classes\PluginManager;
 
 /**
  * Class ItemResource
  *
- * @mixin \Lovata\Shopaholic\Classes\Item\ProductItem
+ * @mixin ProductItem
+ *
  * @package PlanetaDelEste\ApiShopaholic\Classes\Resource\Product
  */
 class ItemResource extends BaseResource
@@ -19,20 +23,20 @@ class ItemResource extends BaseResource
     public function getData(): array
     {
         return [
-            'active'          => (bool)$this->active,
-            'preview_image'   => $this->preview_image ? $this->preview_image->getPath() : null,
-            'images'          => IndexCollectionImages::make(collect($this->images)),
-            'category'        => $this->category ? ItemResourceCategory::make($this->category) : null,
-            'property'        => $this->formatProperty(),
-            'category_name'   => $this->category ? $this->category->name : null,
-            'offers'          => $this->offer->count() ? IndexCollectionOffer::make($this->offer->collect()) : [],
-            'thumbnail'       => $this->preview_image
+            'active'          => fn() => (bool) $this->active,
+            'preview_image'   => fn() => $this->preview_image ? $this->preview_image->getPath() : null,
+            'images'          => fn() => IndexCollectionImages::make(collect($this->images)),
+            'category'        => fn() => $this->category ? ItemResourceCategory::make($this->category) : null,
+            'property'        => fn() => $this->formatProperty(),
+            'category_name'   => fn() => $this->category ? $this->category->name : null,
+            'offers'          => fn() => $this->offer->count() ? IndexCollectionOffer::make($this->offer->collect()) : [],
+            'thumbnail'       => fn() => $this->preview_image
                 ? $this->preview_image->getThumb(300, 300, ['mode' => 'crop'])
                 : null,
-            'secondary_thumb' => $this->images
+            'secondary_thumb' => fn() => $this->images
                 ? collect($this->images)->first()->getThumb(300, 300, ['mode' => 'crop'])
                 : null,
-            'brand'           => $this->brand ? ItemResourceBrand::make($this->brand) : null
+            'brand'           => fn() => $this->brand ? ItemResourceBrand::make($this->brand) : null
         ];
     }
 
@@ -61,6 +65,7 @@ class ItemResource extends BaseResource
     protected function formatProperty(): array
     {
         $arProperties = [];
+
         if (PluginManager::instance()->exists('Lovata.PropertiesShopaholic')) {
             $arProperties = $this->property->toSimpleArray();
         }
